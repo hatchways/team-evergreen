533940038f8b58ce69a554b4906e532221f5eb11
Fil <filgambatesa@gmail.com>
2019-08-25 20:24:21 -0400

    Remove console.log statement

--- server/config/db-connect.js
+++ server/config/db-connect.js
@@ -6,7 +6,6 @@ const config = require('./config.js');
 const mongoose = require('mongoose');
 
 const dbUrl = `${config.db.dbType}://${config.db.host}:${config.db.port}/${config.db.name}`;
-console.log (dbUrl);
 const dbConnect = () => {
   return mongoose.connect(dbUrl)
 };
