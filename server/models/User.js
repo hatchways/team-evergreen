e0f06ed03cbce047358f7c625e78e821dffc0d70
Lamanova <lamanovavi21@gmail.com>
2019-09-02 14:34:44 -0700

    Upload photo to S3

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,21 @@
 //User.js
 //Created by Fil Gambatesa - August 25, 2019
 
+require('mongoose-type-url');
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-                                   name: String,
-                                   email: {
-                                     type: String,
-                                     required: true,
-                                     unique: true
-                                   },
-                                  password: {
-                                     type: String,
-                                     required: true
-                                  }
-                                 });
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: String
+});
 
 module.exports = mongoose.model('user', userSchema);
