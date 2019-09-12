601d1e30ea5366553455cc4b8f159ae0eb60b445
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-09-12 15:42:03 -0400

    Starting files for feature.

---
+++ server/routes/api/vote.js
@@ -0,0 +1,18 @@
+//vote.js
+
+const express = require("express");
+const router = express.Router();
+
+// External Modules
+
+/**
+ * @desc Registers or updates a users vote depending on whether or not a vote
+ * @desc was previously cast.
+ */
+router.post("/vote", function(req, res) {
+    // no files uploaded
+});
+
+//PRIVATE FUNCTIONS
+
+module.exports = router;
---
+++ server/routes/utils/voteModelUpdates.js
@@ -0,0 +1,11 @@
+//voteModelUpdates
+/**
+ * @author - Fil
+ * @date - September 8, 2019
+ * @purpose - Functions used to cast a vote
+ */
+
+import vote from "../../models/Vote";
+import poll from "../../models/Poll";
+
+export async function registerVote(data, res) {}
