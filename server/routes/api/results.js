c461decb85c301e2ff09977b2ff9d4121b298bdd
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-09-15 11:51:51 -0400

    Returns votes without user info.

--- server/routes/api/results.js
+++ server/routes/api/results.js
@@ -1,6 +1,33 @@
 //results.js
 
 const express = require("express");
-const router = express.router;
+const router = express.Router();
+
+//External Modules
+import { getVotes } from "../utils/getVotes";
+
+router.get("/results", (req, res) => {
+    if (req.body === null) {
+        res.send({ status: 400, error: "No poll id provided" });
+    } else {
+        try {
+            getVotes(req.body.pollId)
+                .then(result => {
+                    res.send(result);
+                })
+                .catch(err => {
+                    console.log("Call to getVotes returned error:", err);
+                    res.send(err);
+                });
+        } catch (err) {
+            console.log("/api/poll/results", err);
+            res.send({
+                status: 500,
+                error: "/api/poll/results failure",
+                err
+            });
+        }
+    }
+});
 
 module.exports = router;
--- server/routes/utils/getVotes.js
+++ server/routes/utils/getVotes.js
@@ -2,6 +2,16 @@
 
 const Vote = require("../../models/Vote");
 
-export async function getVotes(pollId) {}
+export async function getVotes(pollId) {
+    await Vote.find({ pollId: pollId }, "option userId")
+        .populate("name avatar")
+        .then(results => {
+            console.log(results);
+            return { status: 200, results: results };
+        })
+        .catch(err => {
+            return { status: 500, error: "Unable to get votes", err };
+        });
+}
 
 // PRIVATE FUNCTIONS
