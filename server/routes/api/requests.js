98c1851f58a6e730fce21540a4eff913bfa91c20
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-09-17 16:10:36 -0400

    Completes the requests api.

--- server/routes/api/requests.js
+++ server/routes/api/requests.js
@@ -5,11 +5,16 @@ const router = express.Router();
 
 //External Modules
 
+import { getRequests } from "../utils/getFriendsPollRequests";
+
 router.get("/requests", (req, res) => {
-    if (req.query.pollId === undefined) {
-        res.status(400).json({ error: "No poll id provided" });
+    if (req.query.userId === undefined) {
+        res.status(400).json({ error: "No user id provided" });
     } else {
         try {
+            getRequests(req.query.userId).then(result => {
+                res.status(200).json(result);
+            });
         } catch (err) {
             console.log("/api/poll/requests", err);
             res.status(500).json({
