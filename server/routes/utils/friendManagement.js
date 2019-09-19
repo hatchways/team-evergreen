48269d1148c6f37d5d31c5808284dc4daeb71b8f
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-09-19 18:23:46 -0400

    Enables un-follow route

--- server/routes/utils/friendManagement.js
+++ server/routes/utils/friendManagement.js
@@ -22,7 +22,23 @@ export async function followUser(userId, friendId) {
     }
 }
 
-export async function unFollowUser(userId, friendId) {}
+export async function unFollowUser(userId, friendId) {
+    try {
+        await User.findByIdAndUpdate(
+            { _id: userId },
+            {
+                $pull: { friends: friendId }
+            }
+        ).exec();
+        return { userId, friendId };
+    } catch (err) {
+        console.log(`${userId} attempting to un-follow ${friendId}`, err);
+        return {
+            status: 500,
+            error: "Error occurred while trying to remove friend."
+        };
+    }
+}
 
 export async function randomSuggestions(userId) {
     return "Random Suggestions";
