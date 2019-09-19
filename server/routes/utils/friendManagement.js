9647ea842aa891342f46221221e56d2b3d0ee40d
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-09-19 18:14:04 -0400

    Enables adding of a friend via route.

--- server/routes/utils/friendManagement.js
+++ server/routes/utils/friendManagement.js
@@ -5,9 +5,25 @@ import User from "../../models/User";
 //Add user to friends list
 
 export async function followUser(userId, friendId) {
-    return "follow";
+    try {
+        await User.findByIdAndUpdate(
+            { _id: userId },
+            {
+                $push: { friends: friendId }
+            }
+        ).exec();
+        return { userId, friendId };
+    } catch (err) {
+        console.log(`${userId} attempting to follow ${friendId}`, err);
+        return {
+            status: 500,
+            error: "Error occurred while trying to add friend."
+        };
+    }
 }
 
-export async function unFollowUser(userId, friendId) {
-    return { o: "follow" };
+export async function unFollowUser(userId, friendId) {}
+
+export async function randomSuggestions(userId) {
+    return "Random Suggestions";
 }
