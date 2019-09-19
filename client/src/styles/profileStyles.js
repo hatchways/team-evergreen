165336692a11ddf72292d64633abf6e561b7e1fb
Olga Fomin <olga.fomin26@gmail.com>
2019-09-19 14:22:26 -0700

    Fix minor styling issues

--- client/src/styles/profileStyles.js
+++ client/src/styles/profileStyles.js
@@ -88,9 +88,16 @@ export const profileStyles = theme => ({
             flexShrink: 0
         }
     },
-    sliderControls: {
+    sliderControl: {
+        padding: "5px",
         position: "absolute",
         top: "50%",
-        transform: "translateY(-50%)"
+        transform: "translateY(-50%)",
+        "& .MuiIcon-root": {
+            fontSize: "2.5rem"
+        }
+    },
+    nextButton: {
+        right: 0
     }
 });
