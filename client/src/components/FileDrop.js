fd910a9248d0c106876954f5d9874d3b3fa5102e
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-10-01 09:52:09 -0400

    Remove warnings.

--- client/src/components/FileDrop.js
+++ client/src/components/FileDrop.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { Typography } from "@material-ui/core";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import initialDropzoneImage from "../images/dropzone300.png";
 
@@ -45,12 +44,12 @@ export function FileDrop(props) {
     const [dropzoneImage, setDropzoneImage] = useState(initialDropzoneImage);
     const { option, setImageFile } = props;
     const classes = useStyles();
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    const { getRootProps, getInputProps } = useDropzone({
         accept: "image/*",
         multiple: false,
         onDrop: acceptedFiles => {
             setImageFile(acceptedFiles, option);
-            acceptedFiles.map(file => {
+            acceptedFiles.forEach(file => {
                 Object.assign(file, {
                     preview: URL.createObjectURL(file)
                 });
