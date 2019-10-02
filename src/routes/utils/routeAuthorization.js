cad0a8f8f36009102885e42294c22bdb01619c4a
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-10-02 10:34:44 -0400

    Adds 401 response for unauthorized requests

--- src/routes/utils/routeAuthorization.js
+++ src/routes/utils/routeAuthorization.js
@@ -3,8 +3,11 @@
 export function sayHello(req, res, next) {
     console.log(req.body, req.ip, req.method);
     if (!req.body.jwt && req.method !== "GET") {
-        res.status(400).json({ error: "Unauthorized access request" });
-        return;
+        //res.status(400).json({ error: "Unauthorized access request" });
+        //return { status: 400, error: "Unauthorized access" };
+        res.status(401)
+            .json({ error: "Unauthorized access request" })
+            .end();
     }
     next();
 }
