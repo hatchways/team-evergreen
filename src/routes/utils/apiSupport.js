ff6bb0e34276ef4451db24fa261158638e14e46d
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-11-21 11:26:13 -0500

    Comments the function.

--- src/routes/utils/apiSupport.js
+++ src/routes/utils/apiSupport.js
@@ -1,5 +1,14 @@
 //apiSupport.js
-
+/*
+ * @desc Executes data requests query through the api
+ * @param req - express route data object
+ * @param res - callback function with results from route
+ * @param route - string value of the route
+ * @param getData - function to execute to get data
+ * @param opts - array of data required to execute query - options should be in
+ * @param the order required by the data getter - opts[0] needs to be the key Id
+ * @param used to make decisions about getting data
+ */
 export function executeApiRequest(req, res, route, getData, opts) {
     if (req.query.userId === undefined) {
         res.status(400).json({ error: "No user id provided" });
