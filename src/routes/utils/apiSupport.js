dff845d5f3ad1c451bca6409f421ae3505253b51
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-11-21 09:58:49 -0500

    Updates executeApiRequest to allow for multiple values

--- src/routes/utils/apiSupport.js
+++ src/routes/utils/apiSupport.js
@@ -1,11 +1,11 @@
 //apiSupport.js
 
-export function executeApiRequest(req, res, route, getData) {
+export function executeApiRequest(req, res, route, getData, opts) {
     if (req.query.userId === undefined) {
         res.status(400).json({ error: "No user id provided" });
     } else {
         try {
-            getData(req.query.userId).then(result => {
+            getData(...opts).then(result => {
                 res.status(200).json(result);
             });
         } catch (err) {
