2ca79c5c98c52e8394f74cbafc8f568297b5516e
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-11-21 12:02:06 -0500

    Refactors follow route to use executeApiRequest

--- src/routes/api/friends.js
+++ src/routes/api/friends.js
@@ -13,18 +13,10 @@ import { executeApiRequest } from "../utils/apiSupport";
 
 // Follows friend
 router.post("/follow", function(req, res) {
-    if (req.body.userId === undefined) {
-        res.status(400).json({ error: "No data provided." });
-    } else {
-        followUser(req.body.userId, req.body.friendId)
-            .then(result => {
-                res.status(200).json(result);
-            })
-            .catch(err => {
-                console.log("/api/friends/follow", err);
-                res.status(500).json({ error: "Unable to follow friend", err });
-            });
-    }
+    executeApiRequest(req, res, "/api/friends/follow", followUser, [
+        req.body.userId,
+        req.body.friendId
+    ]);
 });
 
 // Unfollows friend
