008947938478969ee0f6cc75aeb4b6e43796fae5
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-11-21 09:47:44 -0500

    Refactors module to use centralized api request function

--- src/routes/api/requests.js
+++ src/routes/api/requests.js
@@ -4,24 +4,11 @@ const express = require("express");
 const router = express.Router();
 
 //External Modules
-
+import { executeApiRequest } from "../utils/apiSupport";
 import { getRequests } from "../utils/getFriendsPollRequests";
 
 router.get("/requests", (req, res) => {
-    if (req.query.userId === undefined) {
-        res.status(400).json({ error: "No user id provided" });
-    } else {
-        try {
-            getRequests(req.query.userId).then(result => {
-                res.status(200).json(result);
-            });
-        } catch (err) {
-            console.log("/api/poll/requests", err);
-            res.status(500).json({
-                error: "/api/poll/requests failure"
-            });
-        }
-    }
+    executeApiRequest(req, res, "/api/poll/requests", getRequests);
 });
 
 module.exports = router;
