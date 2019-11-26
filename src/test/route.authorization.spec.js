4e72d1754060adb3d95a0238142048fe37df5507
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-11-26 16:22:21 -0500

    Wip

--- src/test/route.authorization.spec.js
+++ src/test/route.authorization.spec.js
@@ -12,17 +12,34 @@ import { describe } from "mocha";
 import { expect } from "chai";
 
 describe("Authorization framework works", function() {
-    const payload = { name: "Test Token" };
     const secret = "ThisIsASecret";
 
     it("Should return a token if all fields provided", function(done) {
+        const payload = { name: "Test Token" };
         const res = {
             json: obj => {
                 expect(obj.status === 200).to.be.true;
                 expect(obj.token !== "").to.be.true;
+                done();
             }
         };
-        let result = createToken(payload, res, secret, 31556926);
+        createToken(payload, res, secret);
+    });
+
+    it("Should return an error if not all fields are provided", function(done) {
+        const payload = "";
+        const res = {
+            status: 600,
+            json: obj => {
+                console.log(obj, res.status);
+                res.status = obj.status;
+                expect(obj.status === 500).to.be.true;
+                expect(obj.error === "Unable to generate token.").to.be.true;
+                return obj;
+            }
+        };
+        createToken(payload, res, secret);
+        console.log(res);
         done();
     });
 });
