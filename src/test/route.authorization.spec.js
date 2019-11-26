94552c0b48e09bf11437ede37d2fc1c2c0e984d2
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-11-26 17:15:27 -0500

    Tests for missing secret key.

--- src/test/route.authorization.spec.js
+++ src/test/route.authorization.spec.js
@@ -12,7 +12,7 @@ import { describe } from "mocha";
 import { expect } from "chai";
 
 describe("Authorization framework works", async function() {
-    const secret = "ThisIsASecret";
+    let secret = "ThisIsASecret";
 
     it("Should return a token if all fields provided", function(done) {
         const payload = { name: "Test Token" };
@@ -34,6 +34,25 @@ describe("Authorization framework works", async function() {
 
     it("Should fail if payload not an object", function(done) {
         const payload = "Test Token";
+        const res = {
+            status: 0,
+            token: null,
+            error: null,
+            json: obj => {
+                res.status = obj.status;
+                res.token = obj.token;
+                res.error = obj.error;
+                expect(res.status === 500).to.be.true;
+                expect(res.error === "Unable to generate token.").to.be.true;
+                done();
+            }
+        };
+        createToken(payload, res, secret, 10);
+    });
+
+    it("Should fail if secret has no value", function(done) {
+        const payload = "Test Token";
+        secret = "";
         const res = {
             status: 0,
             token: null,
