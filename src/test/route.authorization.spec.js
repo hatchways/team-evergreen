0b0792f0b4c4c416a842206f24c1a59b4a9d36b7
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-11-26 17:09:30 -0500

    Tests for token creation.

--- src/test/route.authorization.spec.js
+++ src/test/route.authorization.spec.js
@@ -11,35 +11,24 @@ import {
 import { describe } from "mocha";
 import { expect } from "chai";
 
-describe("Authorization framework works", function() {
+describe("Authorization framework works", async function() {
     const secret = "ThisIsASecret";
 
     it("Should return a token if all fields provided", function(done) {
         const payload = { name: "Test Token" };
         const res = {
+            status: 0,
+            token: null,
+            error: null,
             json: obj => {
-                expect(obj.status === 200).to.be.true;
-                expect(obj.token !== "").to.be.true;
+                res.status = obj.status;
+                res.token = obj.token;
+                res.error = obj.error;
+                expect(res.status === 200).to.be.true;
+                expect(res.token !== null).to.be.true;
                 done();
             }
         };
-        createToken(payload, res, secret);
-    });
-
-    it("Should return an error if not all fields are provided", function(done) {
-        const payload = "";
-        const res = {
-            status: 600,
-            json: obj => {
-                console.log(obj, res.status);
-                res.status = obj.status;
-                expect(obj.status === 500).to.be.true;
-                expect(obj.error === "Unable to generate token.").to.be.true;
-                return obj;
-            }
-        };
-        createToken(payload, res, secret);
-        console.log(res);
-        done();
+        createToken(payload, res, secret, 10);
     });
 });
