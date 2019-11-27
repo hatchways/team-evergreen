678c77c84efd821c4b291b5b2026dae3d6bcb300
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-11-26 19:39:42 -0500

    Adds test for friendList validation module.

---
+++ src/test/friendList.validation.spec.js
@@ -0,0 +1,32 @@
+//friendList.validation.spic.js
+
+// Module Being Tested
+import { validateFriendListInput } from "../validation/friendList";
+
+// Test Framework
+import { describe } from "mocha";
+import { expect } from "chai";
+import { createToken } from "../routes/utils/routeAuthorization";
+
+describe("Friend list validation works", async function() {
+    it("Should return an error when list title is empty", function(done) {
+        const data = { title: null };
+        const result = validateFriendListInput(data);
+        expect(result.isValid).to.be.false;
+        done();
+    });
+
+    it("Should return an error when friends array is empty", function(done) {
+        const data = { title: "Test Friend List", friends: [] };
+        const result = validateFriendListInput(data);
+        expect(result.isValid).to.be.false;
+        done();
+    });
+
+    it("Should be valid when all data is provided", function(done) {
+        const data = { title: "Test Friend List", friends: ["Friend"] };
+        const result = validateFriendListInput(data);
+        expect(result.isValid).to.be.true;
+        done();
+    });
+});
